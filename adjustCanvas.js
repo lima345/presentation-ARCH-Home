7e1f69bb96bb0c62e9a1c5c151b52e828007b961
Matheus de Lima <59211395+lima345@users.noreply.github.com>
2025-05-12 00:19:17 -0300

    Add files via upload

---
+++ adjustCanvas.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var canvas = document.getElementById("unity-canvas");
+    canvas.style.width = "1920px";
+    canvas.style.height = "1080px";
+});
